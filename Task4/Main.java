169dccaa770f12dcf917d8a005e9eb2ec9869c61
khat0076 <khattab2003@live.com>
2025-09-24 15:47:19 +0200

    GameMenu.class

---
+++ Task4/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+class Main{
+	
+	
+	public static void main(String[] args){
+
+	ArrayList<String> actions = new ArrayList<>();
+
+	actions.add("1) Start game");
+	actions.add("2) Resume game");
+	actions.add("3) Pause game");
+	actions.add("4) End game");
+
+	GameMenu gameMenus = new GameMenu(actions);
+
+	gameMenus.displayMenu();
+
+
+
+	}
+
+
+}
